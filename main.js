1495c334a1fd05345c877aa4de674768d18bfb20
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2021-12-19 22:34:44 -0900

    Add files via upload

--- main.js
+++ main.js
@@ -10,5 +10,39 @@ function modelReady() {
 
 function gotResults(error, results) {
     console.log("Function Got Result Verified");
+    if (error) {
+        console.error(error)
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
 
+        document.getElementById("result_label").innerHTML = "I can hear - " +
+            results[0].label;
+        document.getElementById("result_confidence").innerHTML = "Accuracy - " +
+            (results[0].confidence * 100).toFixed(2) + " %";
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_r + ")";
+        document.getElementById("result_confidence").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_r + ")";
+
+        img = document.getElementById('animal');
+
+        if (results[0].label == "Background Noise") {
+            img.src = 'background.jpg';
+
+        } else if (results[0].label == "Meow") {
+            img.src = 'Cat.jpg';
+
+        } else if (results[0].label == "Roaring") {
+            img.src = 'Lion.jpg';
+
+        } else if (results[0].label == "barking animal") {
+            img.src = 'Dog.jpg';
+
+
+        } else {
+            img.src = 'Cow.jpg';
+
+        }
+    }
 }
